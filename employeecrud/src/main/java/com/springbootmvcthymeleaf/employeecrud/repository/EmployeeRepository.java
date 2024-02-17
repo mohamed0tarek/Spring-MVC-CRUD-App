a31ccf49e1977a413da120f5d440fe4801e9d8fb
mohamed0tarek <mohamed.tarek72417293@gmail.com>
2024-02-17 03:24:34 +0200

    Delete employeecrud directory

--- employeecrud/src/main/java/com/springbootmvcthymeleaf/employeecrud/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springbootmvcthymeleaf.employeecrud.repository;
-
-import com.springbootmvcthymeleaf.employeecrud.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
